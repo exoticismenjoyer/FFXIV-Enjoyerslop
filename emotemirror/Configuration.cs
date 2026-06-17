03aece8741a867db21787bcf788ad063ef25be49
exoticismEnjoyer <230001910+exoticismenjoyer@users.noreply.github.com>
2026-06-17 16:20:08 +0300

    Add EnableDebugLogging property to Configuration

--- emotemirror/Configuration.cs
+++ emotemirror/Configuration.cs
@@ -11,7 +11,7 @@ public class Configuration : IPluginConfiguration
     public bool EmoteMirrorEnabled { get; set; } = false;
     public bool EmoteMotionOnly { get; set; } = false;   // appends "motion" to suppress chat message
     public bool EmoteFriendsOnly { get; set; } = false;  // only mirror emotes from friends
-
+    public bool EnableDebugLogging { get; set; } = false;
     public void Save()
     {
         EmoteMirrorPlugin.PluginInterface.SavePluginConfig(this);
